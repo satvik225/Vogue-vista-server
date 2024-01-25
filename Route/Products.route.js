e9a3be2ca043b4b68c925a3aaffd62c440749868
SATVIK DUBEY <satvik@SATVIKs-MacBook-Air.local>
2024-01-26 00:05:12 +0530

    first commit

---
+++ Route/Products.route.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+const {
+  getProductsById,
+  getProducts,
+  shoeProducts,
+  dressesProducts,
+  subCategory,
+  searchNameApi,
+  deleteProducts,
+  updateProducts,
+} = require("../Controller/Products.controller");
+
+const { authorisation } = require("../middleware/middleware");
+
+const productRouter = Router();
+
+productRouter.get("/", getProducts);
+productRouter.get("/shoes", shoeProducts);
+productRouter.get("/dresses", dressesProducts);
+productRouter.get("/search:name", searchNameApi);
+productRouter.get("/:id", getProductsById);
+productRouter.delete("/:id", deleteProducts);
+productRouter.patch("/:id", updateProducts);
+
+module.exports = { productRouter };
